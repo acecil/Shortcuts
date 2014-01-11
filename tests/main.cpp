7f601661c08e537b1b82973b6bd219307bfb8c5a
Andrew Cecil <gascoyne@gmail.com>
2014-01-11 22:24:04 +0000

    Run tests as part of build.

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,7 +4,7 @@
 /* Header files containing tests. */
 #include "testConfig.h"
 
-int wmain(int argc, char **argv)
+int wmain(int argc, wchar_t **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();

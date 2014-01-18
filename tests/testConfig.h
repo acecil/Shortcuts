cc90d649e561839d46582edbe2efe7e78aa3fd17
Andrew Cecil <gascoyne@gmail.com>
2014-01-18 11:54:07 +0000

    Add a couple more configuration tests.

--- tests/testConfig.h
+++ tests/testConfig.h
@@ -3,6 +3,8 @@
 
 #include "Config.h"
 
+using std::wstring;
+
 class testConfig : public ::testing::Test
 {
 protected:
@@ -12,5 +14,19 @@ protected:
 
 TEST_F(testConfig, GetParamStringEmpty)
 {
-	EXPECT_EQ(std::wstring(), _config.GetParam<std::wstring>(L"None"));
+	EXPECT_EQ(wstring(), _config.GetParam<wstring>(L"None"));
+}
+
+TEST_F(testConfig, GetParamStringEmptyDefault)
+{
+	wstring def(L"Default Value");
+	EXPECT_EQ(def, _config.GetParam<wstring>(L"None", def));
+}
+
+TEST_F(testConfig, SetGetParamString)
+{
+	wstring name(L"TestParam");
+	wstring value(L"Test Value");
+	_config.SetParam<wstring>(name, value);
+	EXPECT_EQ(value, _config.GetParam<wstring>(name));
 }
